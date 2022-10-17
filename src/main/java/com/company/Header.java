f4f39bb59f49ebeb550059c79a98db3497742c1b
Krystian M <krystian2007@gmail.com>
2022-10-17 13:06:35 +0200

    added-method-that-returns-list-of-headers-names

--- src/main/java/com/company/Header.java
+++ src/main/java/com/company/Header.java
@@ -1,16 +1,25 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Header {
+
     private final String FIRST_COLUMN_NAME;
     private final String SECOND_COLUMN_NAME;
     private final String THIRD_COLUMN_NAME;
     private final String FOURTH_COLUMN_NAME;
 
-    public Header(String first_column_name, String second_column_name, String third_column_name, String fourth_column_name) {
+    public Header() {
         FIRST_COLUMN_NAME = "COMPANY";
         SECOND_COLUMN_NAME = "Q1";
         THIRD_COLUMN_NAME = "Q2";
         FOURTH_COLUMN_NAME = "Q3";
+    }
+
+    public List<String> getHeaderNames(){
+        return Arrays.asList(FIRST_COLUMN_NAME, SECOND_COLUMN_NAME, THIRD_COLUMN_NAME, FOURTH_COLUMN_NAME);
     }
 
 }
