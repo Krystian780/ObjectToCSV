d87a9d36f157f7b511f4332a41ba1280d0d07272
Krystian M <krystian2007@gmail.com>
2022-10-17 12:18:12 +0200

    added=generic-class-that-contains-names-of-columns

---
+++ src/main/java/com/company/Header.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Header {
+    private final String FIRST_COLUMN_NAME;
+    private final String SECOND_COLUMN_NAME;
+    private final String THIRD_COLUMN_NAME;
+    private final String FOURTH_COLUMN_NAME;
+
+    public Header(String first_column_name, String second_column_name, String third_column_name, String fourth_column_name) {
+        FIRST_COLUMN_NAME = "COMPANY";
+        SECOND_COLUMN_NAME = "Q1";
+        THIRD_COLUMN_NAME = "Q2";
+        FOURTH_COLUMN_NAME = "Q3";
+    }
+
+}
